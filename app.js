92ac3d44f72fe64b1776c21779425ebdb75a6e0c
Batuhan <yesiltas_sibel@hotmail.com>
2022-03-09 11:03:38 +0300

    js

---
+++ app.js
@@ -0,0 +1,25 @@
+let btn=document.querySelector("#switch");
+let container=document.querySelectorAll(".container")
+
+
+// btn.addEventListener("click",()=>{
+
+//     body.innerHTML = `<div class="container" style="background-color:white">`
+//     body.setAttribute("style","background-color:black")
+    
+
+    
+// })
+
+
+
+
+// document.getElementById("gecis").addEventListener("click", function(){
+//     document.getElementsByTagName('body')[0].classList.toggle("karanlik-mod");
+// });
+
+
+
+document.getElementById("gecis").addEventListener("click", function(){
+    document.getElementsByTagName('body')[0].classList.toggle("karanlik-mod");
+});
